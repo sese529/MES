1dbffe27fd7debfb1e702f6f5ab62d3606ac90c1
hdj185 <hdj185583@gmail.com>
2023-05-16 11:37:18 +0900

    공정(Mprocess) 엔티티 수정, 원자재(Materials), 제품(Product) 엔티티 생성

---
+++ src/main/java/com/B1team/b01/entity/Materials.java
@@ -0,0 +1,32 @@
+package com.B1team.b01.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="MATERIALS")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Materials {
+    @Id
+    @Column(name = "mtr_id", nullable = false, columnDefinition = "number")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mtr_seq_generator")
+    @SequenceGenerator(name = "mtr_seq_generator", sequenceName = "PROCESS_SEQ", initialValue = 1, allocationSize = 1)
+    private Long id;        //원자재 고유번호
+
+    @Column(name="mtr_name", nullable = false, columnDefinition = "varchar2(20)")
+    private String name;    //자재명
+
+    @Column(name="mtr_location", nullable = false, columnDefinition = "varchar2(20)")
+    private String location;    //창고위치
+
+    @Column(name="mtr_price", nullable = false, columnDefinition = "varchar2(20)")
+    private String price;    //구매단가
+
+    @Column(name="mtr_unit", nullable = false, columnDefinition = "varchar2(10)")
+    private String unit;    //단위
+}
--- src/main/java/com/B1team/b01/entity/Product.java
+++ src/main/java/com/B1team/b01/entity/Product.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name="product")
+@Table(name="PROCUCT")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,12 +18,15 @@ public class Product {
     @SequenceGenerator(name = "product_seq_generator", sequenceName = "PROCESS_SEQ", initialValue = 1, allocationSize = 1)
     private Long id;        //제품 고유번호
 
-    @Column(name="product_name", nullable = false, columnDefinition = "varchar2", length = 15)
+    @Column(name="product_name", nullable = false, columnDefinition = "varchar2(15)")
     private String name;    //제품명
 
-    @Column(name="product_price", nullable = false, columnDefinition = "number", length = 10)
-    private int price;    //제품명
+    @Column(name="product_price", nullable = false, columnDefinition = "number(10)")
+    private int price;    //가격
 
-    @Column(name="product_sort", nullable = false, columnDefinition = "varchar2", length = 15)
+    @Column(name="product_sort", nullable = false, columnDefinition = "varchar2(15)")
     private String sort;    //분류
+
+    @Column(name="product_unit", nullable = false, columnDefinition = "varchar2(10)")
+    private String unit;    //단위
 }
