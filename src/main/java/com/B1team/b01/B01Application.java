e7cee7ad3721b652723b289cd4c03d7242bf3dc0
찌인 <119689104+mcektzja@users.noreply.github.com>
2023-05-16 09:34:06 +0900

    Update B01Application.java

--- src/main/java/com/B1team/b01/B01Application.java
+++ src/main/java/com/B1team/b01/B01Application.java
@@ -1,6 +1,6 @@
 package com.B1team.b01;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
