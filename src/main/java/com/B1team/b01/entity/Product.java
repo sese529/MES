c763caafcf9443582cd804a5937762ec7d1f7e33
hdj185 <hdj185583@gmail.com>
2023-05-16 10:41:46 +0900

    제품(product) 엔티티 생성

---
+++ src/main/java/com/B1team/b01/entity/Product.java
@@ -0,0 +1,29 @@
+package com.B1team.b01.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="product")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    @Column(name = "product_id", nullable = false, columnDefinition = "number")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq_generator")
+    @SequenceGenerator(name = "product_seq_generator", sequenceName = "PROCESS_SEQ", initialValue = 1, allocationSize = 1)
+    private Long id;        //제품 고유번호
+
+    @Column(name="product_name", nullable = false, columnDefinition = "varchar2", length = 15)
+    private String name;    //제품명
+
+    @Column(name="product_price", nullable = false, columnDefinition = "number", length = 10)
+    private int price;    //제품명
+
+    @Column(name="product_sort", nullable = false, columnDefinition = "varchar2", length = 15)
+    private String sort;    //분류
+}
