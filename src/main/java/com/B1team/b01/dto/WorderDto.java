85696af90ac4ebdc83d4f8ab11227e920d43aea5
찌인 <119689104+mcektzja@users.noreply.github.com>
2023-05-17 16:32:38 +0900

    (Worder관련) 테이블 컬럼추가, dto추가, (wplan관련) repository, service 추가

---
+++ src/main/java/com/B1team/b01/dto/WorderDto.java
@@ -0,0 +1,20 @@
+package com.B1team.b01.dto;
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorderDto {
+    public String id;   //작업지시 고유번호
+    public String processId; //공정 고유번호
+    public String wplanId; //작업계획 고유번호
+    public String facilityId; //설비정보 고유번호
+    public LocalDateTime finishDate; //작업 완료 일자
+
+}
---
+++ src/test/java/com/B1team/b01/WplanRepositoryTests.java
@@ -0,0 +1,16 @@
+package com.B1team.b01;
+
+
+import com.B1team.b01.repository.WplanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class WplanRepositoryTests {
+    @Autowired private WplanRepository wplanRepository;
+
+
+}
