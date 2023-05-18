335024a952867727e3cbf3a8c81e61bf2974d780
찌인 <119689104+mcektzja@users.noreply.github.com>
2023-05-18 14:33:25 +0900

    (Wplan 관련) dto 계획수량 타입변경
    실수 수정

--- src/main/java/com/B1team/b01/dto/WplanDto.java
+++ src/main/java/com/B1team/b01/dto/WplanDto.java
@@ -14,7 +14,7 @@ public class WplanDto {
     public String id;   //작업계획 고유번호
     public String productId; //제품 고유번호
     public String orderId; //수주 고유번호
-    public String cnt; //계획수량
+    public Long cnt; //계획수량
     public LocalDateTime startDate; //계획 시작 일자
     public LocalDateTime endDate; //계획 완료 일자
     public String state; //작업 완료 일자
