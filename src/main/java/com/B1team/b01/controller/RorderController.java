a1e17ec940cb103f9fa73b9f7ba40174f93a232c
hdj185 <hdj185583@gmail.com>
2023-05-22 19:13:10 +0900

    수주 - 뷰 컨트롤러 매핑중

--- src/main/java/com/B1team/b01/controller/RorderController.java
+++ src/main/java/com/B1team/b01/controller/RorderController.java
@@ -30,13 +30,25 @@ public class RorderController {
                         String orderId,
                         String customerName,
                         String productName,
-                        LocalDateTime startDeadline,
-                        LocalDateTime endDeadLine) {
+                        String startDeadline,
+                        String endDeadLine) {
+        //날짜 관련 변환
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime start = startDate == null ? null : LocalDate.parse(startDate, formatter).atStartOfDay();
         LocalDateTime end = endDate == null ? null : LocalDate.parse(endDate, formatter).atTime(23, 59, 59);
-        List<RorderDto> list = rorderService.searchRorder(start, end, orderId, customerName, productName, startDeadline, endDeadLine);
+        LocalDateTime startDeadLineDate = startDeadline == null ? null : LocalDate.parse(startDeadline, formatter).atStartOfDay();
+        LocalDateTime endDeadLineDate = endDeadLine == null ? null : LocalDate.parse(endDeadLine, formatter).atTime(23, 59, 59);
+
+        List<RorderDto> list = rorderService.searchRorder(start, end, orderId, customerName, productName, startDeadLineDate, endDeadLineDate);
+
         model.addAttribute("rorderList", list);
+        model.addAttribute("startDate", startDate);
+        model.addAttribute("endDate", endDate);
+        model.addAttribute("orderId", orderId);
+        model.addAttribute("customerName", customerName);
+        model.addAttribute("productName", productName);
+        model.addAttribute("startDeadline", startDeadline);
+        model.addAttribute("endDeadLine", endDeadLine);
         return "rorder/order";
     }
 }
