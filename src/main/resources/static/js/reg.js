7e7e3eae7354cef37eae188c7b5c004ba7c9bf15
영민 <0158yyj@gmail.com>
2023-05-22 11:22:39 +0900

    html, css, js, controller 추가했습니다

---
+++ src/main/resources/static/js/reg.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("regClose")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$(document).ready(function () {
+  $("#datepicker").datepicker();
+});
