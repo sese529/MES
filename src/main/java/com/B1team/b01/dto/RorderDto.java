8e3ed4af302b612eb67b82da119cb6ac0ff8205f
hdj185 <hdj185583@gmail.com>
2023-05-22 17:23:37 +0900

    RorderDto LocalDateTime->String 모델매퍼 커스텀

--- src/main/java/com/B1team/b01/dto/RorderDto.java
+++ src/main/java/com/B1team/b01/dto/RorderDto.java
@@ -1,10 +1,13 @@
 package com.B1team.b01.dto;
 
+import com.B1team.b01.config.CustomModelMapper;
 import com.B1team.b01.entity.Rorder;
 import lombok.*;
 import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @ToString
@@ -14,13 +17,13 @@ import java.time.LocalDateTime;
 public class RorderDto {
 
     public String id;
-    public LocalDateTime date;
+    public String date;
     public String customerId;
     public String customerName;
-    public LocalDateTime deadline;
+    public String deadline;
     public Long price;
     public String state;
-    public LocalDateTime changedate;
+    public String changedate;
     public String productId;
     public Long cnt;
     public String productName;
@@ -29,18 +32,30 @@ public class RorderDto {
     private static ModelMapper modelMapper = new ModelMapper(); //엔티티랑 dto의 필드명이 같은 것끼리 매핑
 
 
+
     //Order 엔티티를 RorderDto로 변환해주는 메소드
-    public static RorderDto of(Rorder rorder) {
-        return modelMapper.map(rorder, RorderDto.class);
-    }
+    public static RorderDto of(Rorder rorder) { return CustomModelMapper.getModelMapper().map(rorder, RorderDto.class); }
 
     //RorderDto를 Order 엔티티로 변환해주는 메소드
     public Rorder toEntity() {
         return modelMapper.map(this, Rorder.class);
     }
+
+    // List<Rorder>를 List<RorderDto>로 변환해주는 메소드
+    public static List<RorderDto> of(List<Rorder> rorders) {
+        List<RorderDto> rorderDtos = new ArrayList<>();
+        for (Rorder rorder : rorders) {
+            rorderDtos.add(of(rorder));
+        }
+        return rorderDtos;
+    }
+
+    //List<RorderDto>를 List<Rorder>로 변환해주는 메소드
+    public static List<Rorder> toEntity(List<RorderDto> dtoList) {
+        List<Rorder> entitys = new ArrayList<>();
+        for (RorderDto dto : dtoList) {
+            entitys.add(dto.toEntity());
+        }
+        return entitys;
+    }
 }
-
-/*
-
-1.
- */
