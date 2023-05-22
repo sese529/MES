98adf324221468b7d7ee30a75fde282e226a699a
hdj185 <121775135+hdj185@users.noreply.github.com>
2023-05-23 00:00:01 +0900

    수주 - 레포지토리 save 성공

--- src/main/java/com/B1team/b01/dto/RorderFormDto.java
+++ src/main/java/com/B1team/b01/dto/RorderFormDto.java
@@ -3,11 +3,13 @@ package com.B1team.b01.dto;
 import com.B1team.b01.entity.Rorder;
 import lombok.*;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @ToString
@@ -23,26 +25,25 @@ public class RorderFormDto {
     public Long cnt;
     public Long price;
     public String state;
-    public String date;
-    public String changedate;
-    public String deadline;
+    public LocalDateTime date;
+    public LocalDateTime changedate;
+    public LocalDateTime deadline;
 
     private static ModelMapper modelMapper = new ModelMapper(); //엔티티랑 dto의 필드명이 같은 것끼리 매핑
 
-    @PersistenceContext private EntityManager entityManager;
+//    @Autowired private EntityManager entityManager;
 
     //RorderFormDto를 Order 엔티티로 변환해주는 메소드
     public Rorder toEntity() {
-        this.setId(generateId("ROR", "order_seq"));
+//        this.setId(generateId("ROD", "order_seq"));
         return modelMapper.map(this, Rorder.class);
     }
 
     //문자열 시퀀스 메소드
-    @Transactional
-    public String generateId(String head, String seqName) {
-        BigDecimal sequenceValue = (BigDecimal) entityManager.createNativeQuery("SELECT " + seqName + ".NEXTVAL FROM dual").getSingleResult();
-        String id = head + sequenceValue;
-        return id;
-
-    }
+//    @Transactional
+//    public String generateId(String head, String seqName) {
+//        BigDecimal sequenceValue = (BigDecimal) entityManager.createNativeQuery("SELECT " + seqName + ".NEXTVAL FROM dual").getSingleResult();
+//        String id = head + sequenceValue;
+//        return id;
+//    }
 }
