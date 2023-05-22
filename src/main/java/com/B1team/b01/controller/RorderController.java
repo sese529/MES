d1b6040f5e87661347e8c1c56f6ab31814eaf799
hdj185 <hdj185583@gmail.com>
2023-05-22 18:54:01 +0900

    수주 페이지 - 컨트롤러 날짜 수정

--- src/main/java/com/B1team/b01/controller/RorderController.java
+++ src/main/java/com/B1team/b01/controller/RorderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -24,15 +25,16 @@ public class RorderController {
 
     @GetMapping("/")
     public String order(Model model,
-                        LocalDate startDate,
-                        LocalDate endDate,
+                        String startDate,
+                        String endDate,
                         String orderId,
                         String customerName,
                         String productName,
                         LocalDateTime startDeadline,
                         LocalDateTime endDeadLine) {
-        LocalDateTime start = startDate == null ? null : startDate.atStartOfDay();
-        LocalDateTime end = endDate == null ? null : endDate.atTime(23, 59, 59);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime start = startDate == null ? null : LocalDate.parse(startDate, formatter).atStartOfDay();
+        LocalDateTime end = endDate == null ? null : LocalDate.parse(endDate, formatter).atTime(23, 59, 59);
         List<RorderDto> list = rorderService.searchRorder(start, end, orderId, customerName, productName, startDeadline, endDeadLine);
         model.addAttribute("rorderList", list);
         return "rorder/order";
