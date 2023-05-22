4a4325ef7355edd101ff95d77e80ae5df9c29195
sugyeong <asg7824@naver.com>
2023-05-22 10:19:45 +0900

    계산 controller

--- src/main/java/com/B1team/b01/Controller/StockController.java
+++ src/main/java/com/B1team/b01/Controller/StockController.java
@@ -1,10 +1,12 @@
 package com.B1team.b01.Controller;
 
+import com.B1team.b01.Service.BomService;
 import com.B1team.b01.Service.StockService;
 import com.B1team.b01.entity.Order;
 import com.B1team.b01.entity.Stock;
 import com.B1team.b01.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,16 +23,28 @@ import java.util.Optional;
 @RequestMapping("/stock/*")
 public class StockController {
 
+    @Autowired
     private final StockService stockService;
 
+    @Autowired
+    private final BomService bomService;
+
     @GetMapping("")
-    public String getCompare(@PathVariable String productId, String orderId, Model model) {
-        //재고량 & 주문량 비교
+    public String getCompare(@PathVariable String productId, String orderId, Model model, String pid, double amount) {
+        //재고량 & 주문량 비교 - 재고량 < 주문량 -> 발주계산
         stockService.stockCheck(productId,orderId);
+
+        //발주 계산
+        bomService.calcBom(pid,amount);
+
+        //자동 발주
+
+
+
 
 //
 //        model.addAllAttributes()
-        return "index";
+        return "test";
 
     }
 }
