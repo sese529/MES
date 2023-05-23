9cf5a8096bf440632b313ab4088fbc156f375c4c
sese529 <neno5678@gmail.com>
2023-05-23 10:37:04 +0900

    생산 계획 완료(html은 미완료)

--- src/main/java/com/B1team/b01/controller/ProductionController.java
+++ src/main/java/com/B1team/b01/controller/ProductionController.java
@@ -1,52 +1,89 @@
 package com.B1team.b01.controller;
 
-import com.B1team.b01.service.ProductionService;
+import com.B1team.b01.entity.Product;
 import com.B1team.b01.entity.Wplan;
+import com.B1team.b01.service.ProductService;
+import com.B1team.b01.service.ProductionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Controller
-@Transactional
 @RequiredArgsConstructor
 @RequestMapping("/production/*")
 public class ProductionController {
-    private ProductionService productionService;
-
     @Autowired
-    public ProductionController(ProductionService productionService) {
-        this.productionService = productionService;
-    }
+    private ProductionService productionService;
+    @Autowired
+    private ProductService productService;
+
 
     @GetMapping("production-order")
     public String getOrderList(Model model, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
         List<Wplan> wlist = productionService.getAllWplan();
         model.addAttribute("wlist", wlist);
+        List<Product> plist = productService.getAllProduct();
+        model.addAttribute("plist",plist);
 
         return "production/production-order";
     }
 
-    @GetMapping("search")
+    @PostMapping("/search")
     public String wplanSearch(Model model,
                               @RequestParam(required = false) String id,
                               @RequestParam(required = false) String orderId,
                               @RequestParam(required = false) String state,
-                              @RequestParam(required = false) LocalDateTime min,
-                              @RequestParam(required = false) LocalDateTime max){
-        List<Wplan> searchlist = productionService.search(id,orderId,state,min,max);
+                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate min,
+                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate max){
+        LocalDateTime smin = null;
+        LocalDateTime smax = null;
+        if(min != null){
+            smin= LocalDateTime.of(min, LocalTime.MIN);
+        }
+
+        if(max != null){
+            smax = LocalDateTime.of(max, LocalTime.MAX);
+        }
+
+        if("".equals(orderId)){
+            orderId=null;
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.println(id);
+        System.out.println(orderId);
+        System.out.println(state);
+        System.out.println(min);
+        System.out.println(max);
+
+        List<Wplan> searchlist = productionService.search(id,orderId,state, smin, smax);
         model.addAttribute("searchlist",searchlist);
 
+        List<Product> plist = productService.getAllProduct();
+        model.addAttribute("plist",plist);
+
         return "production/production-order";
+    }
+
+    @GetMapping("production-plan")
+    public String getWerderList(Model model, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        List<Wplan> wlist = productionService.getAllWplan();
+        model.addAttribute("wlist", wlist);
+        List<Product> plist = productService.getAllProduct();
+        model.addAttribute("plist",plist);
+
+        return "production/production-plan";
     }
 }
--- src/main/java/com/B1team/b01/repository/ProductRepository.java
+++ src/main/java/com/B1team/b01/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package com.B1team.b01.repository;
 import com.B1team.b01.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product,String> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product,String> {
+    //vnaahr
+    List<Product> findAll(); //전체 품목 리스트 가져오는거
 
 }
