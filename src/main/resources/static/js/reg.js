a7d75f0bfdd409cbf95ed4a4eded480ea07e83fc
hdj185 <hdj185583@gmail.com>
2023-05-23 14:52:07 +0900

    수주 - 등록 시 자동 수주일자 계산(납품일 예측X)

--- src/main/resources/static/js/reg.js
+++ src/main/resources/static/js/reg.js
@@ -3,6 +3,13 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("openModalBtn");
 var span = document.getElementsByClassName("regClose")[0];
 btn.onclick = function () {
+  // 현재 날짜와 시간을 계산
+  var currentDate = new Date();
+  var currentDateTime = currentDate.toLocaleString(); // 날짜와 시간을 문자열로 변환
+
+  // input 요소에 현재 날짜와 시간 설정
+  document.getElementById("regOrderCurrentTimeInput").value = currentDateTime;
+
   modal.style.display = "block";
 }
 span.onclick = function () {
