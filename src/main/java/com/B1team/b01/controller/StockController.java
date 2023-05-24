b62603faa91f82a87a6845692f652c45b806ce9b
영민 <0158yyj@gmail.com>
2023-05-24 17:41:20 +0900

    테이블 스크롤, css, bom 추가

--- src/main/java/com/B1team/b01/controller/StockController.java
+++ src/main/java/com/B1team/b01/controller/StockController.java
@@ -37,6 +37,11 @@ public class StockController {
 
         return "/item/stock";
     }
+
+    @GetMapping("/item/bom")
+    public String BOM() {
+        return "/item/bom";
+    }
 //
 //    @GetMapping("product")
 //    public String mappTest(Model model) {
