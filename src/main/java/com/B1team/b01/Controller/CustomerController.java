634038210f6376087ed90e4df0c93e72336a2816
sese529 <neno5678@gmail.com>
2023-05-24 11:34:18 +0900

    거래처 리스트, 검색 완료

--- src/main/java/com/B1team/b01/Controller/CustomerController.java
+++ src/main/java/com/B1team/b01/Controller/CustomerController.java
@@ -1,36 +1,92 @@
 package com.B1team.b01.controller;
 
+import com.B1team.b01.entity.Customer;
+import com.B1team.b01.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/customer/*")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
     @GetMapping("/customer/rorder-customer")
-    public String rorderCustomer() {
+    public String rorderCustomer(Model model) {
+        String sort = "수주처";
+        model.addAttribute("clist",customerService.getPorderList(sort));
 
         return "/customer/rorder-customer";
     }
 
     @GetMapping("/customer/porder-customer")
-    public String porderCustomer() {
+    public String porderCustomer(Model model) {
+        String sort = "발주처";
+        model.addAttribute("clist",customerService.getPorderList(sort));
 
         return "/customer/porder-customer";
     }
 
     @PostMapping("/pordersearch")
-    public String porderSearch(){
+    public String porderSearch(Model model,
+                               @RequestParam(required = false) String name,
+                               @RequestParam(required = false) String id,
+                               @RequestParam(required = false) String businessNumber){
+        if("".equals(name)){
+            name=null;
+        }
+        if("".equals(id)){
+            id=null;
+        }
+        if("".equals(businessNumber)){
+            businessNumber=null;
+        }
 
-        return null;
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(businessNumber);
+
+        String sort="발주처";
+        List<Customer> slist = customerService.search(name,id,businessNumber,sort);
+        System.out.println(slist);
+        model.addAttribute("slist",slist);
+
+        return "/customer/porder-customer";
     }
 
     @PostMapping("/rordersearch")
-    public String rorderSearch(){
+    public String rorderSearch(Model model,
+                               @RequestParam(required = false) String name,
+                               @RequestParam(required = false) String id,
+                               @RequestParam(required = false) String businessNumber){
+        if("".equals(name)){
+            name=null;
+        }
+        if("".equals(id)){
+            id=null;
+        }
+        if("".equals(businessNumber)){
+            businessNumber=null;
+        }
 
-        return null;
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(businessNumber);
+
+        String sort="수주처";
+        List<Customer> slist = customerService.search(name,id,businessNumber,sort);
+        System.out.println(slist);
+        model.addAttribute("slist",slist);
+
+        return "/customer/rorder-customer";
     }
 }
--- src/main/java/com/B1team/b01/repository/CustomerRepository.java
+++ src/main/java/com/B1team/b01/repository/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.B1team.b01.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> , JpaSpecificationExecutor<Customer> {
+import java.util.List;
 
+public interface CustomerRepository extends JpaRepository<Customer, String> , JpaSpecificationExecutor<Customer> {
+    List<Customer> findBySort(String customerSort);
 }
