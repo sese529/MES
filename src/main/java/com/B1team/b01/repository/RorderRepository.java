51f902af9c0c0058c949ac75aee5c35622568c55
sese529 <neno5678@gmail.com>
2023-05-25 10:22:44 +0900

    오류 수정

--- src/main/java/com/B1team/b01/repository/RorderRepository.java
+++ src/main/java/com/B1team/b01/repository/RorderRepository.java
@@ -15,14 +15,24 @@ public interface RorderRepository extends JpaRepository<Rorder, String> {
             "AND (:endDate IS NULL OR r.date <= :endDate) " +
             "AND (:orderId IS NULL OR r.id = :orderId) " +
             "AND (:state IS NULL OR " +
-                "(:state = '진행전' AND r.state = '미확정' AND :now IS NULL) OR " +
-                "(:state = '진행중' And r.state = '확정' AND r.deadline > :now) OR " +
-                "(:state = '완료' AND r.state = '확정' AND r.deadline <= :now)) " +
+            "(:state = '진행전' AND r.state = '미확정' AND :now IS NULL) OR " +
+            "(:state = '진행중' And r.state = '확정' AND r.deadline > :now) OR " +
+            "(:state = '완료' AND r.state = '확정' AND r.deadline <= :now)) " +
             "AND (:customerName IS NULL OR r.customerName = :customerName) " +
             "AND (:productName IS NULL OR r.productName = :productName) " +
             "AND (:startDeadline IS NULL OR r.deadline >= :startDeadline) " +
             "AND (:endDeadLine IS NULL OR r.deadline <= :endDeadLine) " +
             "ORDER BY r.date DESC")
+    List<Rorder> findRordersByConditions(@Param("startDate") LocalDateTime startDate,
+                                                                         @Param("endDate")LocalDateTime endDate,
+                                                                         @Param("orderId")String orderId,
+                                                                         @Param("state")String state,
+                                                                         @Param("now")LocalDateTime now,
+                                                                         @Param("customerName")String customerName,
+                                                                         @Param("productName")String productName,
+                                                                         @Param("startDeadline")LocalDateTime startDeadline,
+                                                                         @Param("endDeadLine")LocalDateTime endDeadLine);
+
 
 
     Optional<Rorder> findById(String Id);
@@ -30,15 +40,7 @@ public interface RorderRepository extends JpaRepository<Rorder, String> {
     @Query("SELECT ro.cnt FROM Rorder ro WHERE ro.id = :orderId")
     Long findByOrderCnt(String orderId);
 
-    List<Rorder> findRordersByConditions(@Param("startDate") LocalDateTime startDate,
-                                         @Param("endDate")LocalDateTime endDate,
-                                         @Param("orderId")String orderId,
-                                         @Param("state")String state,
-                                         @Param("now")LocalDateTime now,
-                                         @Param("customerName")String customerName,
-                                         @Param("productName")String productName,
-                                         @Param("startDeadline")LocalDateTime startDeadline,
-                                         @Param("endDeadLine")LocalDateTime endDeadLine);
+
 
 
 
