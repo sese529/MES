69ad7064c8876fe1c14e94e708bba9024dc2d711
yeongmins <oxox123456@naver.com>
2023-05-26 06:59:59 +0900

    메인 추가

--- src/main/resources/static/js/orderCurrentChart.js
+++ src/main/resources/static/js/orderCurrentChart.js
@@ -1,59 +1,30 @@
-// 예시 데이터
-var data = [
-    {name: "수주 A", startDate: new Date(2023, 0, 1), endDate: new Date(2023, 0, 10)},
-    {name: "수주 B", startDate: new Date(2023, 0, 5), endDate: new Date(2023, 0, 15)},
-    {name: "수주 C", startDate: new Date(2023, 0, 12), endDate: new Date(2023, 0, 20)},
-    // 추가 데이터 입력
-];
+google.charts.load('current', {'packages': ['gantt']});
+google.charts.setOnLoadCallback(drawChart);
 
-var canvas = document.getElementById("orderCurrent");
-var ctx = canvas.getContext("2d");
+function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('string', 'Resource');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
 
-// Gantt 차트 그리기
-function drawGanttChart(data) {
-    var barHeight = 30; // 막대 높이
-    var barMargin = 10; // 막대 간격
+    data.addRows([
+        ['1', 'ROD30', 'Resource 1', new Date(2023, 4, 1), new Date(2023, 4, 5), null, 100, null],
+        ['2', 'ROD31', 'Resource 2', new Date(2023, 4, 6), new Date(2023, 4, 10), null, 50, null],
+        ['3', 'ROD32', 'Resource 3', new Date(2023, 4, 11), new Date(2023, 4, 15), null, 75, null]
+    ]);
 
-    // 최소 날짜와 최대 날짜 계산
-    var minDate = new Date(Math.min(...data.map(item => item.startDate)));
-    var maxDate = new Date(Math.max(...data.map(item => item.endDate)));
+    var options = {
+        width: 800,
+        height: 400,
+        barHeight: 50
+    };
 
-    // x축과 y축 설정
-    var xAxisStart = 100; // x축 시작 위치
-    var yAxisStart = 50; // y축 시작 위치
-    var chartWidth = canvas.width - xAxisStart - 20; // 차트의 너비
-    var chartHeight = canvas.height - yAxisStart - 20; // 차트의 높이
+    var chart = new google.visualization.Gantt(document.getElementById('orderCurrent'));
 
-    // x축 그리기
-    ctx.beginPath();
-    ctx.moveTo(xAxisStart, yAxisStart + chartHeight);
-    ctx.lineTo(xAxisStart + chartWidth, yAxisStart + chartHeight);
-    ctx.stroke();
-
-    // y축 그리기
-    ctx.beginPath();
-    ctx.moveTo(xAxisStart, yAxisStart);
-    ctx.lineTo(xAxisStart, yAxisStart + chartHeight);
-    ctx.stroke();
-
-    // 막대 그리기
-    for (var i = 0; i < data.length; i++) {
-        var item = data[i];
-        var barY = yAxisStart + (barHeight + barMargin) * i;
-
-        // 막대 그리기
-        ctx.fillStyle = "#2196f3"; // 막대 색상
-        ctx.fillRect(
-            xAxisStart + (item.startDate - minDate) / (maxDate - minDate) * chartWidth,
-            barY,
-            (item.endDate - item.startDate) / (maxDate - minDate) * chartWidth,
-            barHeight
-        );
-
-        // 막대 라벨 그리기
-        ctx.fillStyle = "#000"; // 라벨 색상
-        ctx.fillText(item.name, xAxisStart - 10, barY + barHeight / 2 + 5);
-    }
+    chart.draw(data, options);
 }
-
-drawGanttChart(data);
