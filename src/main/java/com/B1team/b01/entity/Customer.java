e8df123cfc529b0fac604c6e7ee129d57a88c50d
sese529 <neno5678@gmail.com>
2023-05-25 10:16:15 +0900

    거래처 등록 중간 commit

--- src/main/java/com/B1team/b01/entity/Customer.java
+++ src/main/java/com/B1team/b01/entity/Customer.java
@@ -17,23 +17,23 @@ public class Customer {
     @Column(name = "customer_id", nullable = false, columnDefinition = "varchar2(50)")
 //    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq_generator")
     @SequenceGenerator(name = "customer_seq_generator", sequenceName = "customer_seq", initialValue = 10001, allocationSize = 1)
-    private String id;
+    private String id; // 업체 코드
 
     @Column(name="customer_sort", nullable = false, columnDefinition = "varchar2(9)")
-    private String sort;
+    private String sort; //업체 분류
 
     @Column(name="customer_name", nullable = false, columnDefinition = "varchar2(50)")
-    private String name;
+    private String name; //업체명
 
     @Column(name="customer_address", nullable = false, columnDefinition = "varchar2(100)")
-    private String address;
+    private String address; //업체 주소
 
     @Column(name="customer_tel", nullable = false, columnDefinition = "varchar2(15)")
-    private String tel;
+    private String tel; //연락처
 
     @Column(name="customer_business_number", nullable = false, columnDefinition = "varchar2(20)")
-    private String businessNumber;
+    private String businessNumber; //사업자번호
 
     @Column(name="customer_fax", nullable = false, columnDefinition = "varchar2(20)")
-    private String fax;
+    private String fax; //팩스번호
 }
