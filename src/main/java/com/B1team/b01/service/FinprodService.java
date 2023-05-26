e119f25964666c0d0245e9fe04bc52a0f5679e68
hdj185 <hdj185583@gmail.com>
2023-05-26 20:24:19 +0900

    출하 관리 페이지 완료

--- src/main/java/com/B1team/b01/service/FinprodService.java
+++ src/main/java/com/B1team/b01/service/FinprodService.java
@@ -2,6 +2,7 @@ package com.B1team.b01.service;
 
 import com.B1team.b01.dto.FinprodDto;
 
+import com.B1team.b01.dto.ShipmentDto;
 import com.B1team.b01.entity.Rorder;
 
 import com.B1team.b01.repository.FinprodRepository;
@@ -9,10 +10,15 @@ import com.B1team.b01.repository.RorderRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -51,4 +57,17 @@ public class FinprodService {
 
         finprodRepository.save(finprodDto.toEntity());
         }
+
+    //출하관리 페이지 - 완제품에 거래처 추가하여 리스트 얻기
+    //매개변수 String형을 레포지토리 메소드의 매개변수에 맞게 변경하기
+    public List<ShipmentDto> getShipmentList(String customerName,
+                                             String productName,
+                                             String orderId,
+                                             String start,
+                                             String end) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime startDate = start == null || "".equals(start)? null : LocalDate.parse(start, formatter).atStartOfDay();
+        LocalDateTime endDate = end == null || "".equals(end) ? null : LocalDate.parse(end, formatter).atTime(23, 59, 59);
+        return finprodRepository.findShipmentsByConditions(customerName, productName, orderId, startDate, endDate);
     }
+}
