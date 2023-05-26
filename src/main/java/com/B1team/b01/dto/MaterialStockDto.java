5d5c572070fdf6b78f79b79d095eccae8a3aadb0
hdj185 <hdj185583@gmail.com>
2023-05-26 11:52:16 +0900

    자재 관리 - 원자재 재고 레포지토리로 메소드 작성

---
+++ src/main/java/com/B1team/b01/dto/MaterialStockDto.java
@@ -0,0 +1,13 @@
+package com.B1team.b01.dto;
+
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MaterialStockDto {
+    private String name;    //원자재명
+    private Long ea;       //재고량
+}
